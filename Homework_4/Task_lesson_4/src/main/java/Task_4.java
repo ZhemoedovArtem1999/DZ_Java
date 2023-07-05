ddd052ac43ecf7b21bec82cfccf8d170099599a9
Жемоедов Артём Александрович <tema.zhemoedov1999@gmail.com>
2023-07-05 11:25:22 +0300

    Начало работы над 4 дз

---
+++ Homework_4/Task_lesson_4/src/main/java/Task_4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Task_4 {
+
+    static Scanner scanner;
+    public static void main(String[] args) {
+        scanner = new Scanner(System.in);
+        boolean isClose = false;
+        while (!isClose)
+        {
+            System.out.println("Выберите действие:\n1 - Загрузить бд пользователей\n2 - Вывести пользователей\n3 - Сортировка по возрасту\n4 - Сортировка по возрасту и полу\n5 - Выход");
+            int action = scanner.nextInt();
+
+        }
+    }
+}
