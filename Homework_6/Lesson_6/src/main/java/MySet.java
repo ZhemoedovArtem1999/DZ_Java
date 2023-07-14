b9d24778c26dc5e1ac0cc0009fc348157050a297
Жемоедов Артём Александрович <tema.zhemoedov1999@gmail.com>
2023-07-14 11:57:39 +0300

    Дз 6 урок

---
+++ Homework_6/Lesson_6/src/main/java/MySet.java
@@ -0,0 +1,36 @@
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Objects;
+
+public class MySet<T> {
+
+    private LinkedHashMap<T, Object> map;
+    private Object nullValue = new Object();
+
+    public MySet() {
+        map = new LinkedHashMap<T, Object>();
+    }
+
+    public boolean add(T value) {
+        return map.put(value,nullValue) == null;
+    }
+
+    public boolean remove(T value) {
+        return map.remove(value)==nullValue;
+    }
+
+    public T get(int index) {
+         return (T)map.keySet().toArray()[index]; // map.keySet().(index);
+    }
+
+    public String toString()
+    {
+        return map.keySet().toString();
+    }
+
+    public Iterator<T> iterator ()
+    {
+        return  map.keySet().iterator();
+    }
+}
--- Homework_6/Lesson_6/src/main/java/lesson_6.java
+++ Homework_6/Lesson_6/src/main/java/lesson_6.java
@@ -1,41 +1,44 @@
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
 
 public class lesson_6 {
 
     public static void main(String[] args) {
-        MapH<Integer> mapH = new MapH<Integer>();
+        MySet<Integer> setHash = new MySet<Integer>();
+
+        setHash.add(123);
+        setHash.add(1235);
+        setHash.add(123);
+        setHash.add(4525);
+        setHash.add(452);
+        setHash.add(368);
+        setHash.add(2);
+        setHash.add(75);
+        setHash.add(452);
+        setHash.add(563);
+        setHash.add(653);
+        setHash.add(88);
+
+        System.out.println(setHash.toString());
+
+        System.out.println(setHash.get(1));
+
+
+        System.out.println(setHash.toString());
+        setHash.remove(123);
+
+        System.out.println(setHash.toString());
+
+        Iterator iter = setHash.iterator();
+        while (iter.hasNext())
+        {
+            System.out.println(iter.next());
+        }
+
+        System.out.println();
     }
 
-    public class MapH<T>{
 
-        private ArrayList<T> mapList;
-
-        public MapH()
-        {
-            mapList = new ArrayList<T>();
-        }
-        public boolean add(T value)
-        {
-            if (mapList.contains(value))
-            {
-                return  false;
-            }
-            else
-            {
-                mapList.add(value);
-            }
-
-            return true;
-        }
-
-        public String toString()
-        {
-
-            return mapList.toString();
-        }
-
-
-    }
 }
 
 
